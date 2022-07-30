0aa51f7b3458551977602a9b430a0ea31281395a
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-07-30 14:02:25 +0300

    Task #2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,35 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+// --------------------- #1 ---------------
+
+const makeElement = element => {
+  const el = document.createElement('li');
+  el.classList.add('item');
+  el.textContent = element;
+
+  return el;
+};
+
+const elementsMarkUp = ingredients.map(makeElement);
+
+ingredientsList.append(...elementsMarkUp);
+
+// -------------------- #2 ---------------------
+
+// const makeElements = ingredients => {
+//   return ingredients.map(ingredient => {
+//     const el = document.createElement('li');
+//     el.classList.add('item');
+//     el.textContent = ingredient;
+
+//     return el;
+//   });
+// };
+
+// const markUp = makeElements(ingredients);
+
+// ingredientsList.append(...markUp);
