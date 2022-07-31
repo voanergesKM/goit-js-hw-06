77ce5cb5266276d403d1d9834da5764909271da0
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-07-31 15:52:52 +0300

    Task #9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeCollorBtn = document.querySelector('.change-color');
+const colorValue = document.querySelector('.widget span');
+
+changeCollorBtn.addEventListener('click', () => {
+  colorValue.innerHTML = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+});
