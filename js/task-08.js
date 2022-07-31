b4479895357bef66f09965decdd5065102752a96
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-07-31 15:32:15 +0300

    Task #8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formFieldEl = document.querySelector('.login-form');
 
+formFieldEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  const email = e.currentTarget.elements.email.value;
+  const password = e.currentTarget.elements.password.value;
+
+  if (!email || !password) {
+    window.alert('Please, fill in all fields!');
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  e.currentTarget.reset();
+
+  //   const formData = new FormData(e.currentTarget);
+}
