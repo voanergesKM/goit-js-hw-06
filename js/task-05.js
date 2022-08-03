6ac5c20887550d97a5bed492ec462eb3d9bdc870
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-08-03 20:15:11 +0300

    Task #5/6

--- js/task-05.js
+++ js/task-05.js
@@ -1,13 +1,8 @@
 const inputEl = document.querySelector('#name-input');
 const outputEl = document.querySelector('#name-output');
-const defaultOutputEl = 'Anonymous';
 
 inputEl.addEventListener('input', changeOutputName);
 
 function changeOutputName(e) {
-  outputEl.textContent = e.currentTarget.value;
-
-  if (!e.currentTarget.value) {
-    outputEl.textContent = defaultOutputEl;
-  }
+  outputEl.textContent = e.currentTarget.value || 'Anonymous';
 }
--- js/task-06.js
+++ js/task-06.js
@@ -3,11 +3,11 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onEnoughCharacters);
 
 function onEnoughCharacters(e) {
-  inputEl.classList.add('invalid');
-  inputEl.classList.remove('valid');
-
-  if (e.currentTarget.value.length >= inputEl.dataset.length) {
+  if (Number(e.currentTarget.value.length) === Number(inputEl.dataset.length)) {
     inputEl.classList.add('valid');
     inputEl.classList.remove('invalid');
+  } else {
+    inputEl.classList.add('invalid');
+    inputEl.classList.remove('valid');
   }
 }
