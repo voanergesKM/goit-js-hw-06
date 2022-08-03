b2ae5db9c2a3b94457ddd890fdee02deead4d5d3
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-08-03 16:07:00 +0300

    Task 1

--- js/task-01.js
+++ js/task-01.js
@@ -5,9 +5,24 @@ const categoriesQty = catList.length;
 const message = `Number of categories: ${categoriesQty}`;
 console.log(message);
 
-catList.forEach(el => {
-  const catName = el.firstElementChild.textContent;
-  const catQty = el.lastElementChild.childElementCount;
+// ----------------- Var 1 ---------------------------
 
-  console.log(`Category: ${catName} \nElements: ${catQty}`);
-});
+calcTotalCats(catList);
+
+function calcTotalCats() {
+  catList.forEach(el => {
+    const catName = el.firstElementChild.textContent;
+    const catQty = el.lastElementChild.childElementCount;
+
+    console.log(`Category: ${catName} \nElements: ${catQty}`);
+  });
+}
+
+// ----------------- Var 2 -----------------------------
+
+// catList.forEach(el => {
+//   const catName = el.firstElementChild.textContent;
+//   const catQty = el.lastElementChild.childElementCount;
+
+//   console.log(`Category: ${catName} \nElements: ${catQty}`);
+// });
