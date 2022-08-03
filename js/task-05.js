acfef1bbec4c0f597402ee20e887ee26023bc69c
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-08-03 16:29:28 +0300

    Task #5 fixed

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,13 @@
 const inputEl = document.querySelector('#name-input');
 const outputEl = document.querySelector('#name-output');
+const defaultOutputEl = 'Anonymous';
 
 inputEl.addEventListener('input', changeOutputName);
 
 function changeOutputName(e) {
   outputEl.textContent = e.currentTarget.value;
+
+  if (!e.currentTarget.value) {
+    outputEl.textContent = defaultOutputEl;
+  }
 }
