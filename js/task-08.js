72f28e3fdfc6d48f2debb585646ef5c62c89c2e5
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-08-03 17:48:42 +0300

    Task #8 fixed

--- js/task-08.js
+++ js/task-08.js
@@ -12,12 +12,23 @@ function onSubmitForm(e) {
     window.alert('Please, fill in all fields!');
   }
 
+  // ---------------- Var 1 --------------
+
   const formData = {
     email,
     password,
   };
 
-  e.currentTarget.reset();
+  console.log(formData);
 
-  //   const formData = new FormData(e.currentTarget);
+  // ----------------- Var 2 ----------------
+
+  // const formData = new FormData(e.currentTarget);
+  // formData.forEach((value, name) => {
+  //   console.log(`${name}: ${value}`);
+  // });
+
+  if (email && password) {
+    e.currentTarget.reset();
+  }
 }
