141d0e7950c66a99dd393f887adadadb500c7c9f
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-08-03 17:18:05 +0300

    Task #6 fixed

--- js/task-06.js
+++ js/task-06.js
@@ -3,10 +3,11 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onEnoughCharacters);
 
 function onEnoughCharacters(e) {
-  if (e.currentTarget.value.length < inputEl.dataset.length) {
-    inputEl.classList.add('invalid');
-  } else {
-    inputEl.classList.remove('invalid');
+  inputEl.classList.add('invalid');
+  inputEl.classList.remove('valid');
+
+  if (e.currentTarget.value.length >= inputEl.dataset.length) {
     inputEl.classList.add('valid');
+    inputEl.classList.remove('invalid');
   }
 }
